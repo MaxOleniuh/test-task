c9c3618abc27780ab789ba01d95ff97d2e23d23d
oleniuh <maxim.oleniukh@gmail.com>
2023-06-12 10:22:42 +0100

    background and home page competed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,44 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../../shared/Header";
-import { Title } from "./Home.styled";
+import { Title, HomeWrapper } from "./Home.styled";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { IconButton } from "@mui/material";
 export const Home = () => {
-    return (
-        <>
-            <Header />
-                <Title style={{margin:'100px auto'}}>Welcome, user!</Title>
-        </>
-    )
-}
+  const navigate = useNavigate();
+  const handleNavigateTweets = () => {
+    navigate("/tweets");
+  };
+  return (
+    <>
+      <HomeWrapper>
+        <Header />
+        <Title style={{ margin: "80px auto" }}>
+          Welcome, user! To view tweets simply navigate to tweets page
+        </Title>
+        <IconButton>
+          <TwitterIcon
+            color="primary"
+            fontSize="large"
+            onClick={handleNavigateTweets}
+          />
+        </IconButton>
+        <Title style={{ margin: "10px 0 60px 0"  }}>
+          {" "}
+          The page shows cards with users which were generated from backend.
+          Presented activity on the page: if you would like to follow the user,
+          you can click on FOLLOW button. If you need to recall which of the
+          users you are subscribed to and which are not, reload the page once
+          again in your browser. All data about your subscribtions is stored and
+          will be immediately displayed on the page.
+        </Title>
+        <Title style={{ margin: "0" }}>
+          When following or unfollowing the user, the button color and the text
+          inside of it change. To implement the interactivity of the user with
+          the app, Redux state manager was used. For better experience, user can
+          filter the users by: 'All', 'Followed' and 'Unfollowed'. The
+          pagination was used for better website performance.
+        </Title>
+      </HomeWrapper>
+    </>
+  );
+};
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,4 +1,25 @@
 import s from "@emotion/styled";
+import background from "../../images/background.jpg"
 export const Title = s.h1 `
-    color: blue;
+    background: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    from(#ff8a00),
+    to(#e52e71)
+  );
+  background: linear-gradient(176deg, #0075c9, #76c2b6);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-box-decoration-break: clone;
+  box-decoration-break: clone;
+  text-shadow: none;
+`
+export const Image = s.img`
+    width: 100%;
+    heigth: 100%;
+`
+export const HomeWrapper = s.div`
+background-image: url(${background});
 `
